01aa7e793ec41561c13444b5f1cf5121ebbbdd02
hira03 <hira.farman@techlogix.com>
2015-09-07 15:22:45 +0500

    pricelist updated

---
+++ TMX.WebApp/TMX.WebApp/TMX/MasterData/controllers/ModalInstancePricelistCtrl.js
@@ -0,0 +1,39 @@
+﻿angular
+    .module('MasterData')
+    .controller('ModalInstancePricelistCtrl', function ($scope, $modalInstance, items) {
+
+        $scope.mainGridOptions2 = {
+            toolbar: [
+                 { name: "create", text: "Add New Record" }
+            ],
+            
+            columnMenu: true,
+            resizable: true,
+            pageable: {
+                pageSize: 50,
+                pageSizes: [5,10,20],
+                refresh: true,
+                buttonCount: 1
+            },
+            columns: [
+              { field: "Product" },
+              { field: "Price" }
+            ],
+            dataSource: {
+                schema: {
+                    model: { id: "id" }
+                }
+            },
+            editable: true
+        };
+
+
+
+        $scope.ok = function () {
+            $modalInstance.close($scope.selected.item);
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    });
